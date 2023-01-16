04f7ac8a5adee10b69babeb3a73f93f063a9b4ff
kavi <116868545+kavita2913@users.noreply.github.com>
2023-01-16 15:10:08 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ const heading ={
   return (
     <div id="main">
       <h1 style={{color:"green"}}>Success</h1>
-      <h1  id='error'  style={{color: "red"}} style={{fontSize:"50px"}}>Error</h1>
+      <h1  id='error'  style={{color: "red"}}>Error</h1>
     </div>
   )
 }
