1cc9107543a770847ef17845574bd54497826ba8
kavi <116868545+kavita2913@users.noreply.github.com>
2023-01-16 14:55:43 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -8,8 +8,8 @@ const heading ={
 }
   return (
     <div id="main">
-      <h1 style={{color: "red"}}>Success</h1>
-      <h1  id='error'  style={{color: "green"}} >Error</h1>
+      <h1  style={{color: "green"}} >Success</h1>
+      <h1  id='error'  style={{color: "red"}}>Error</h1>
     </div>
   )
 }
