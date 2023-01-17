aab9bf4cf6ba2dc7c0f46d675647a2372d0f2acb
kavi <116868545+kavita2913@users.noreply.github.com>
2023-01-17 18:11:27 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,19 @@
-import React from 'react'
-import '../styles/App.css';
-import styles from '../styles/appStyles.module.css';
+import React from "react";
+import "../styles/App.css";
+import styles from "../styles/appStyles.module.css";
 const App = () => {
-const heading ={
-    
-            
-      
-}
+  const heading = {};
+  color: "green";
+  fontSize: "50px";
+
   return (
     <div id="main">
-      <h1 style={{color:"green"}}>Success</h1>
-      <h1  id='error'  style={{color: "red"}}>Error</h1>
+      <h1 style={{color: "green" , fontSize: "50px"}}>Success</h1>
+      <h1 id="error" style={{ color: "red", fontSize: "50px" }}>
+        Error
+      </h1>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
